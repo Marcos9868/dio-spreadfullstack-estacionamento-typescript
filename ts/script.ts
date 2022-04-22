03353924aa74f4263f91766557592111b7e6113a
Marcos9868 <marcos_m_ferreira@outlook.com>
2022-04-22 13:59:35 -0300

    Adiciona evento que captura os dados de veículo e placa

--- ts/script.ts
+++ ts/script.ts
@@ -1,3 +1,12 @@
 (function() {
-  
-})
+  const $ = (query: string): HTMLInputElement | null => document.querySelector(query)
+
+  $('#cadastrar')?.addEventListener("click", () => {
+    const veiculo = $('#veiculo')?.value
+    const placa = $('#placa')?.value
+    if (!veiculo || !placa) {
+      alert('Dados de Veículo e Placa obrigatórios!')
+      return
+    }
+  })
+})()
