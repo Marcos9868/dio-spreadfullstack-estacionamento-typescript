5c84c710c48a9959afb1751d92ad9cead7b71ca1
Marcos9868 <marcos_m_ferreira@outlook.com>
2022-04-22 15:19:21 -0300

    Adiciona função adicionarVeiculos e interface Veiculo

--- ts/script.ts
+++ ts/script.ts
@@ -1,5 +1,34 @@
+interface Veiculo {
+  veiculo: string,
+  placa: string,
+  entrada: Date
+}
+
 (function() {
   const $ = (query: string): HTMLInputElement | null => document.querySelector(query)
+
+  function patio() {
+    const lerVeiculos = () => {}
+    const adicionarVeiculos = (veiculo: Veiculo) => {
+      const row = document.createElement('tr')
+
+      row.innerHTML = `
+        <td>${veiculo.veiculo}</td>
+        <td>${veiculo.placa}</td>
+        <td>${veiculo.entrada}</td>
+        <td>
+          <button class="delete" data-placa="${veiculo.placa}">X</button>
+        </td>
+      `
+
+      $("#patio")?.appendChild(row)
+    }
+    const removerVeiculos = () => {}
+    const salvarVeiculos = () => {}
+    const renderVeiculos = () => {}
+
+    return { lerVeiculos, adicionarVeiculos, removerVeiculos, salvarVeiculos, renderVeiculos }
+  }
 
   $('#cadastrar')?.addEventListener("click", () => {
     const veiculo = $('#veiculo')?.value
